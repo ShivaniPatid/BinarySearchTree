59164bc09d807e3cf519cb5c6fec2e7d388d3b2c
ShivaniPatid <shivanipatidar712@gmail.com>
2022-09-16 17:30:56 +0530

    [Shivani] UC1 - Create a binary search tree

--- BinarySearchTreeExample/Program.cs
+++ BinarySearchTreeExample/Program.cs
@@ -10,6 +10,28 @@ namespace BinarySearchTreeExample
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Choose a option");
+            Console.WriteLine("1. UC1 - Create a Binary Search Tree");
+            int option=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            switch (option)
+            {
+                case 1:
+                    UCOneBinarySearchTree<int> binarySearchTree1 = new UCOneBinarySearchTree<int>(56);
+                    binarySearchTree1.Insert(30);
+                    binarySearchTree1.Insert(70);
+                    binarySearchTree1.Display();
+                    break;
+                default:
+                    Console.WriteLine("Choose a appropriate option");
+                    break;
+            }
+
+
+
+
+            Console.ReadLine();
         }
+
     }
 }
---
+++ BinarySearchTreeExample/UCOneBinarySearchTree.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTreeExample
+{
+    public class UCOneBinarySearchTree<T> where T : IComparable<T>
+    {
+        public T NodeData { get; set; }
+        public UCOneBinarySearchTree<T> LeftTree { get; set; }
+        public UCOneBinarySearchTree<T> RightTree { get; set; }
+        public UCOneBinarySearchTree(T nodeData)
+        {
+            this.NodeData = nodeData;
+            this.LeftTree = null;
+            this.RightTree = null; 
+        }
+        int leftCount = 0;
+        int rightCount = 0;
+        public void Insert(T item)
+        {
+            T currentNodeValue = this.NodeData;
+            if((currentNodeValue.CompareTo(item)) > 0)
+            {
+                if (this.LeftTree == null)
+                    this.LeftTree = new UCOneBinarySearchTree<T>(item);
+                else
+                    this.LeftTree.Insert(item);
+            }
+            else
+            {
+                if (this.RightTree == null)
+                    this.RightTree = new UCOneBinarySearchTree<T>(item);
+                else
+                    this.RightTree.Insert(item);
+            }
+        }
+
+        public void Display()
+        {
+            if(this.LeftTree != null)
+            {
+                this.leftCount++;
+                this.LeftTree.Display();
+            }
+            Console.WriteLine(this.NodeData.ToString());
+            if (this.RightTree != null)
+            {
+                this.rightCount++;
+                this.RightTree.Display();
+            }
+        }
+       
+    }
+}
