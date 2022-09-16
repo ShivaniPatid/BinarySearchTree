c10ffd4ef4af4cffaee0bcc96da4a277db50f604
ShivaniPatid <shivanipatidar712@gmail.com>
2022-09-16 18:16:07 +0530

    [Shivani] UC3 - search elememt in binary tree

--- BinarySearchTreeExample/Program.cs
+++ BinarySearchTreeExample/Program.cs
@@ -13,6 +13,7 @@ namespace BinarySearchTreeExample
             Console.WriteLine("Choose a option");
             Console.WriteLine("1. UC1 - Create a Binary Tree");
             Console.WriteLine("2. UC2 - Create a Binary Tree");
+            Console.WriteLine("3. UC3 - Search the element in binary tree");
             int option=Convert.ToInt32(Console.ReadLine());
             Console.WriteLine();
             switch (option)
@@ -39,6 +40,24 @@ namespace BinarySearchTreeExample
                     binarySearchTree2.Insert(67);
                     binarySearchTree2.Display();
                     binarySearchTree2.GetSize();
+                    break;
+                case 3:
+                    UCThreeBinarySearchTree<int> binarySearchTree3 = new UCThreeBinarySearchTree<int>(56);
+                    binarySearchTree3.Insert(30);
+                    binarySearchTree3.Insert(70);
+                    binarySearchTree3.Insert(22);
+                    binarySearchTree3.Insert(40);
+                    binarySearchTree3.Insert(60);
+                    binarySearchTree3.Insert(95);
+                    binarySearchTree3.Insert(11);
+                    binarySearchTree3.Insert(65);
+                    binarySearchTree3.Insert(3);
+                    binarySearchTree3.Insert(16);
+                    binarySearchTree3.Insert(63);
+                    binarySearchTree3.Insert(67);
+                    binarySearchTree3.Display();
+                    binarySearchTree3.GetSize();
+                    bool result = binarySearchTree3.IfExists(63, binarySearchTree3);
                     break;
                 default:
                     Console.WriteLine("Choose a appropriate option");
---
+++ BinarySearchTreeExample/UCThreeBinarySearchTree.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTreeExample
+{
+    public class UCThreeBinarySearchTree<T> where T : IComparable<T>
+    {
+        public T NodeData { get; set; }
+        public UCThreeBinarySearchTree<T> LeftTree { get; set; }
+        public UCThreeBinarySearchTree<T> RightTree { get; set; }
+        public UCThreeBinarySearchTree(T nodeData)
+        {
+            this.NodeData = nodeData;
+            this.LeftTree = null;
+            this.RightTree = null;
+        }
+        int leftCount = 0;
+        int rightCount = 0;
+        public void Insert(T item)
+        {
+            T currentNodeValue = this.NodeData;
+            if ((currentNodeValue.CompareTo(item)) > 0)
+            {
+                if (this.LeftTree == null)
+                    this.LeftTree = new UCThreeBinarySearchTree<T>(item);
+                else
+                    this.LeftTree.Insert(item);
+            }
+            else
+            {
+                if (this.RightTree == null)
+                    this.RightTree = new UCThreeBinarySearchTree<T>(item);
+                else
+                    this.RightTree.Insert(item);
+            }
+        }
+
+        public void Display()
+        {
+            if (this.LeftTree != null)
+            {
+                this.leftCount++;
+                this.LeftTree.Display();
+            }
+            Console.WriteLine(this.NodeData.ToString());
+            if (this.RightTree != null)
+            {
+                this.rightCount++;
+                this.RightTree.Display();
+            }
+        }
+
+        public void GetSize()
+        {
+            Console.WriteLine("Size" + " : " + (1 + this.leftCount + this.rightCount));
+        }
+
+        public bool IfExists(T element, UCThreeBinarySearchTree<T> node)
+        {
+            
+            if (node == null)
+            {
+                Console.WriteLine("Element not found in Binary Search Tree");
+                return false;
+            }
+            if (node.NodeData.Equals(element))
+            {
+                Console.WriteLine("Found the element in Binary Search Tree  " + node.NodeData);
+                return true;
+                
+            }
+            else
+            {
+                Console.WriteLine("Current element is {0} in binary Search Tree",node.NodeData);
+            }
+            if(element.CompareTo(node.NodeData) < 0)
+            {
+                IfExists(element, node.LeftTree); 
+            }
+            if(element.CompareTo(node.NodeData) > 0)
+            {
+                IfExists(element, node.RightTree);
+            }
+            return true;
+        }
+    }
+}
