871b510de770c47e490cb1e751dff97733634760
ShivaniPatid <shivanipatidar712@gmail.com>
2022-09-16 17:47:49 +0530

    [Shivani] UC2 - create a binary tree

--- BinarySearchTreeExample/Program.cs
+++ BinarySearchTreeExample/Program.cs
@@ -11,7 +11,8 @@ namespace BinarySearchTreeExample
         static void Main(string[] args)
         {
             Console.WriteLine("Choose a option");
-            Console.WriteLine("1. UC1 - Create a Binary Search Tree");
+            Console.WriteLine("1. UC1 - Create a Binary Tree");
+            Console.WriteLine("2. UC2 - Create a Binary Tree");
             int option=Convert.ToInt32(Console.ReadLine());
             Console.WriteLine();
             switch (option)
@@ -21,6 +22,23 @@ namespace BinarySearchTreeExample
                     binarySearchTree1.Insert(30);
                     binarySearchTree1.Insert(70);
                     binarySearchTree1.Display();
+                    break;
+                case 2:
+                    UCTwoBinarySearchTree<int> binarySearchTree2 = new UCTwoBinarySearchTree<int>(56);
+                    binarySearchTree2.Insert(30);
+                    binarySearchTree2.Insert(70);
+                    binarySearchTree2.Insert(22);
+                    binarySearchTree2.Insert(40);
+                    binarySearchTree2.Insert(60);
+                    binarySearchTree2.Insert(95);
+                    binarySearchTree2.Insert(11);
+                    binarySearchTree2.Insert(65);
+                    binarySearchTree2.Insert(3);
+                    binarySearchTree2.Insert(16);
+                    binarySearchTree2.Insert(63);
+                    binarySearchTree2.Insert(67);
+                    binarySearchTree2.Display();
+                    binarySearchTree2.GetSize();
                     break;
                 default:
                     Console.WriteLine("Choose a appropriate option");
